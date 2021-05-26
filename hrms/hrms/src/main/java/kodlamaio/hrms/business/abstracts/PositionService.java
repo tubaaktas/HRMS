700e2da578430245f6c87e663bc734ee91c82202
Tuğba <74349907+tubaaktas@users.noreply.github.com>
2021-05-26 21:45:09 +0300

    Delete hrms/hrms directory

--- hrms/hrms/src/main/java/kodlamaio/hrms/business/abstracts/PositionService.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.business.abstracts;
-
-import java.util.List;
-
-import kodlamaio.hrms.entities.concretes.Position;
-
-public interface PositionService {
-	List<Position> getAll();
-}
